c9424c85062ab2b2ca2c1d153d1af05077dcc56d
Miguel Ricaño <mricanho@gmail.com>
2021-01-27 13:06:20 -0600

    Connect user with tweeet

--- db/migrate/20210127161959_add_field_to_users.rb
+++ db/migrate/20210127161959_add_field_to_users.rb
@@ -1,7 +1,7 @@
-class AddFieldToUsers < ActiveRecord::Migration[6.1]
+class AddFieldsToUsers < ActiveRecord::Migration[6.1]
   def change
-    add_column :users, :name, :string
-    add_column :users, :username, :string 
-    add_index :users, :username, unique: true
+  	add_column :users, :name, :string
+  	add_column :users, :username, :string
+  	add_index :users, :username, unique: true
   end
 end
